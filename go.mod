92bc0c45ed99cc518eb4414252a634d29b699797
dezhiShen <1179551960@qq.com>
2021-03-05 17:30:23 +0800

    init:初始化项目

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dezhiShen/MiraiGo-module-plus
+
+go 1.15
+
+require github.com/Logiase/MiraiGo-Template v0.0.0-20210228150851-29092d4d5486 // indirect
---
+++ miraigo-module-plus.go
@@ -0,0 +1,72 @@
+package module
+
+import (
+	"sync"
+
+	"github.com/Mrs4s/MiraiGo/client"
+	"github.com/Mrs4s/MiraiGo/message"
+
+	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Logiase/MiraiGo-Template/utils"
+)
+
+func init() {
+	bot.RegisterModule(instance)
+}
+
+var instance = &ar{}
+var logger = utils.GetModuleLogger("logiase.autoreply")
+var tem map[string]string
+
+type ar struct {
+}
+
+func (a *ar) MiraiGoModule() bot.ModuleInfo {
+	return bot.ModuleInfo{
+		ID:       "dezhiShen.reply",
+		Instance: instance,
+	}
+}
+
+func (a *ar) Init() {
+
+}
+
+func (a *ar) PostInit() {
+}
+
+func (a *ar) Serve(b *bot.Bot) {
+	b.OnGroupMessage(func(c *client.QQClient, msg *message.GroupMessage) {
+		//获取群号,获取回复内容,回复
+		out := autoreply(msg.ToString())
+		if out == "" {
+			return
+		}
+		m := message.NewSendingMessage().Append(message.NewText(out))
+		c.SendGroupMessage(msg.GroupCode, m)
+	})
+
+	b.OnPrivateMessage(func(c *client.QQClient, msg *message.PrivateMessage) {
+		out := autoreply(msg.ToString())
+		if out == "" {
+			return
+		}
+		m := message.NewSendingMessage().Append(message.NewText(out))
+		c.SendPrivateMessage(msg.Sender.Uin, m)
+	})
+}
+
+func (a *ar) Start(bot *bot.Bot) {
+}
+
+func (a *ar) Stop(bot *bot.Bot, wg *sync.WaitGroup) {
+	defer wg.Done()
+}
+
+func autoreply(in string) string {
+	out, ok := tem[in]
+	if !ok {
+		return ""
+	}
+	return out
+}
