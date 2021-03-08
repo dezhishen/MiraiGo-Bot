106468dfc5e93c852a41519a114aa9ee67fc8965
dezhiShen <1179551960@qq.com>
2021-03-08 11:22:20 +0800

    mod(mian):启动项目

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"os"
+	"os/signal"
+
+	"github.com/dezhiShen/MiraiGo-Bot/pkg/migrations"
+
+	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Logiase/MiraiGo-Template/config"
+	"github.com/Logiase/MiraiGo-Template/utils"
+
+	_ "github.com/dezhiShen/MiraiGo-Bot/module"
+)
+
+func init() {
+	utils.WriteLogToFS()
+	config.Init()
+	migrations.Sync("./migrations")
+}
 func main() {
-	print("hello world!")
+	// 快速初始化
+	bot.Init()
+	// 初始化 Modules
+	bot.StartService()
+	// 使用协议
+	// 不同协议可能会有部分功能无法使用
+	// 在登陆前切换协议
+	bot.UseProtocol(bot.AndroidPhone)
+	// 登录
+	bot.Login()
+	// 刷新好友列表，群列表
+	bot.RefreshList()
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, os.Kill)
+	<-ch
+	bot.Stop()
 }
