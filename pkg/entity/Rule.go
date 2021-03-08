19e28af3a7f97df776d71220539ac13896c3d0ad
dezhiShen <1179551960@qq.com>
2021-03-08 15:14:49 +0800

    feat(entity):增加rule

---
+++ pkg/entity/Rule.go
@@ -0,0 +1,12 @@
+package entity
+
+// Rule 规则
+type Rule struct {
+	ID           string
+	Name         string
+	Type         string
+	RespTemplate string
+	Command      string
+	Min          int64
+	Max          int64
+}
