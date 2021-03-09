d5f9371874b17811037a352bd5c5dcfab00667c3
dezhiShen <1179551960@qq.com>
2021-03-09 14:40:10 +0800

    mod(api):修改CallApi的逻辑

--- pkg/engine/api.go
+++ pkg/engine/api.go
@@ -2,8 +2,10 @@ package engine
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/dezhiShen/MiraiGo-Bot/pkg/entity"
 	"github.com/dezhiShen/MiraiGo-Bot/tools"
@@ -11,20 +13,28 @@ import (
 
 // CallAPI api访问
 func CallAPI(api *entity.API, context map[string]interface{}) (string, error) {
-	if api.Method == entity.HTTPGet {
-		res, err := http.Get(tools.ParseTpl(api.URI, context))
-		if err != nil {
-			return "", err
-		}
-		robots, err := ioutil.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			return "", err
-		}
-		resp := string(robots)
-		return processAPIResp(resp, api)
+	var r *http.Response
+	var err error
+	var resp string
+	url := tools.ParseTpl(api.URI, context)
+	if entity.HTTPGet == api.Method {
+		r, err = http.DefaultClient.Get(url)
+	} else if entity.HTTPPost == api.Method {
+		body := tools.ParseTpl(api.RequestTemplate, context)
+		r, err = http.DefaultClient.Post(url, "application/json", strings.NewReader(body))
+	} else {
+		return "", errors.New("不支持的请求格式,当前只支持get和post")
 	}
-	return "", nil
+	if err != nil {
+		return "", err
+	}
+	robots, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	resp = string(robots)
+	return processAPIResp(resp, api)
 }
 
 func processAPIResp(resp string, api *entity.API) (string, error) {
