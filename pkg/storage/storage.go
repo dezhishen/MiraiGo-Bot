884ed2be4e0a88c91c08a06321d492722d50ae22
dezhiShen <1179551960@qq.com>
2021-03-11 14:23:09 +0800

    feat(storage):增加根据前缀遍历游标的操作

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"bytes"
+
 	"github.com/boltdb/bolt"
 )
 
@@ -38,5 +40,19 @@ func Delete(bucket, key string) error {
 		b := tx.Bucket([]byte(bucket))
 		err := b.Delete([]byte(key))
 		return err
+	})
+}
+
+// GetByPrefix 根据前缀获取
+func GetByPrefix(bucket, prefix string, decoder func(string, string) error) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(bucket)).Cursor()
+		for k, v := c.Seek([]byte(prefix)); k != nil && bytes.HasPrefix(k, []byte(prefix)); k, v = c.Next() {
+			err := decoder(string(k), string(v))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	})
 }
