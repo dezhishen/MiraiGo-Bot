bb5806ac64475ca6028e64b4660ef01a6ad01de8
dezhiShen <1179551960@qq.com>
2021-03-12 17:15:21 +0800

    fix(storage):直接传递[]byte

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -18,48 +18,48 @@ func init() {
 }
 
 // Get 获取
-func Get(bucket, key string, decoder func(string) error) error {
+func Get(bucket, key []byte, decoder func([]byte) error) error {
 	return db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
+		b := tx.Bucket(bucket)
 		if b == nil {
-			return decoder("")
+			return decoder(nil)
 		}
-		v := b.Get([]byte(key))
-		return decoder(string(v))
+		v := b.Get(key)
+		return decoder(v)
 	})
 }
 
 // Put 放入
-func Put(bucket, key, value string) error {
+func Put(bucket, key, value []byte) error {
 	return db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte(bucket))
-		b := tx.Bucket([]byte(bucket))
-		err := b.Put([]byte(key), []byte(value))
+		tx.CreateBucketIfNotExists(bucket)
+		b := tx.Bucket(bucket)
+		err := b.Put(key, []byte(value))
 		return err
 	})
 }
 
 // Delete 删除
-func Delete(bucket, key string) error {
+func Delete(bucket, key []byte) error {
 	return db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte(bucket))
-		b := tx.Bucket([]byte(bucket))
-		err := b.Delete([]byte(key))
+		tx.CreateBucketIfNotExists(bucket)
+		b := tx.Bucket(bucket)
+		err := b.Delete(key)
 		return err
 	})
 }
 
 // GetByPrefix 根据前缀获取
-func GetByPrefix(bucket, prefix string, decoder func(string, string) error) error {
+func GetByPrefix(bucket, prefix []byte, decoder func([]byte, []byte) error) error {
 	return db.Update(func(tx *bolt.Tx) error {
-		// tx.CreateBucketIfNotExists([]byte(bucket))
-		b := tx.Bucket([]byte(bucket))
+		// tx.CreateBucketIfNotExists(bucket)
+		b := tx.Bucket(bucket)
 		if b == nil {
 			return nil
 		}
 		c := b.Cursor()
-		for k, v := c.Seek([]byte(prefix)); k != nil && bytes.HasPrefix(k, []byte(prefix)); k, v = c.Next() {
-			err := decoder(string(k), string(v))
+		for k, v := c.Seek(prefix); k != nil && bytes.HasPrefix(k, prefix); k, v = c.Next() {
+			err := decoder(k, v)
 			if err != nil {
 				return err
 			}
@@ -69,11 +69,11 @@ func GetByPrefix(bucket, prefix string, decoder func(string, string) error) erro
 }
 
 // HasByPrefix 根据前缀判断是否存在
-func HasByPrefix(bucket, prefix string) (bool, error) {
+func HasByPrefix(bucket, prefix []byte) (bool, error) {
 	var r bool
 	err := db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte(bucket))
-		c := tx.Bucket([]byte(bucket)).Cursor()
+		tx.CreateBucketIfNotExists(bucket)
+		c := tx.Bucket(bucket).Cursor()
 		for k, _ := c.Seek([]byte(prefix)); k != nil && bytes.HasPrefix(k, []byte(prefix)); k, _ = c.Next() {
 			r = true
 		}
@@ -83,19 +83,19 @@ func HasByPrefix(bucket, prefix string) (bool, error) {
 }
 
 // Incr
-func Incr(bucket, key string, incr int) (int, error) {
+func Incr(bucket, key []byte, incr int) (int, error) {
 	var r int
 	err := db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte(bucket))
-		b := tx.Bucket([]byte(bucket))
-		v := b.Get([]byte(key))
+		tx.CreateBucketIfNotExists(bucket)
+		b := tx.Bucket(bucket)
+		v := b.Get(key)
 		if v == nil {
 			r = incr
 			// v = IntToBytes(incr)
 		} else {
 			r = BytesToInt(v) + incr
 		}
-		err := b.Put([]byte(key), IntToBytes(r))
+		err := b.Put(key, IntToBytes(r))
 		return err
 	})
 	return r, err
